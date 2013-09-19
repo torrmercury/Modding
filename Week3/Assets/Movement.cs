7addd61b49d6f72efc133e58acac6277d5cee6ed
Matt Chen <matt.chen312@gmail.com>
2013-09-19 12:14:35 -0400

    Worked on Brick Breaker piece of crap

---
+++ Week3/Assets/Movement.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(10f,0f,0f) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += new Vector3(0f, 0f, 10f) * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += new Vector3(-10f, 0f, 0f) * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += new Vector3(0f, 0f, -10f) * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(0f, 0f, 10f) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(10f, 0f, 0f) * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += new Vector3(-10f, 0f, 0f) * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += new Vector3(0f, 0f, -10f) * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(-10f, 0f, 0f) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(10f, 0f, 0f) * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += new Vector3(0f, 0f, 10f) * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += new Vector3(0f, 0f, -10f) * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0f, 0f, -10f) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(10f, 0f, 0f) * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += new Vector3(0f, 0f, 10f) * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += new Vector3(-10f, 0f, 0f) * Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Week3/Assets/Sphere.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    rigidbody.AddForce (500f, -500f, 500f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
