0739a5c079c669b2e62bc666c1484dd1714f5d1b
Matt Chen <matt.chen312@gmail.com>
2013-09-19 10:35:24 -0400

    Added Week 3 files.

---
+++ Week2/Assets/Scripts/animate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animate : MonoBehaviour {
+    public float x = 1f;
+    public float y = 1f;
+    public float z = 1f;
+
+	// Use this for initialization
+	void Start () {
+        //if 
+        //{
+            //x = .5f;
+            //y = .5f;
+            //z = .5f;
+        //}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > 2f && Time.time <7f)
+        {
+            transform.position = transform.position + new Vector3(x, y, z) * Time.deltaTime;
+            transform.localScale += new Vector3(x, y, z) * Time.deltaTime;
+            transform.Rotate(0f, 0f, 45f * Time.deltaTime);
+        }
+        if (Time.time < 2f && Time.time > 7f)
+        {
+            transform.position += new Vector3(x, y, z) * Time.deltaTime;
+            transform.localScale += new Vector3(x, y, z) * Time.deltaTime;
+            transform.Rotate(0f, 0f, 45f * Time.deltaTime);
+        }
+
+        if (Time.time >= 5f && light.enabled == false)
+        {
+            light.enabled = true;
+        }
+	}
+}
---
+++ Week2/Assets/Scripts/cammove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cammove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(1f, 1f, 1f) * Time.deltaTime;
+        transform.localScale += new Vector3(1f, 1f, 1f) * Time.deltaTime;
+    }
+}
