df5f956244c954c9a62019d50763814f1b08a404
torrmercury <matt.chen312@gmail.com>
2013-10-01 02:00:53 -0400

    Made my aquarium. HW week 4 DONE

---
+++ Week4HW/Assets/Scripts/fishgod.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class fishgod : MonoBehaviour {
+    public fishscript fishBlueprint;
+    public fishscript fishBlueprint2;
+    public int fishCount = 75;
+    public List<fishscript> fishlist = new List<fishscript>();
+    int randomNum = 0;
+	// Use this for initialization
+
+	void Start () {
+        int currentfishcounter = 0;
+        while (currentfishcounter < fishCount)
+        {
+            randomNum = Random.Range(0, 100);
+            if (randomNum < 50)
+            {
+                Vector3 fishPosition = Random.insideUnitSphere * 20f;
+                fishscript newFish = Instantiate(fishBlueprint, fishPosition, Quaternion.identity) as fishscript;
+                fishlist.Add(newFish);
+                currentfishcounter++;
+            }
+            else
+            {
+                Vector3 fishPosition = Random.insideUnitSphere * 20f;
+                fishscript newFish = Instantiate(fishBlueprint2, fishPosition, Quaternion.identity) as fishscript;
+                fishlist.Add(newFish);
+                currentfishcounter++;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (fishscript currentFish in fishlist)
+            {
+                currentFish.destination = Vector3.zero;
+            }
+	    }
+    }
+}
---
+++ Week4HW/Assets/Scripts/fishscript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishscript : MonoBehaviour {
+    public Vector3 destination = new Vector3(0f, 0f, 0);
+    public float speed = 5f;
+	// Use this for initialization
+	void Start () 
+    {
+        InvokeRepeating("SetNewDestination", 0f, 15f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.LookRotation(rigidbody.velocity);
+	}
+    void FixedUpdate()
+    {
+        Vector3 direction = Vector3.Normalize(destination - transform.position);
+        rigidbody.AddForce(direction * speed, ForceMode.Acceleration);
+    }
+
+    void SetNewDestination()
+    {
+        destination = Random.insideUnitSphere * 30f;
+    }
+}
