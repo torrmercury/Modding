79d5f0ace006acc92a606c72833a73c7f3eb6e2c
Matt Chen <matt.chen312@gmail.com>
2013-10-09 18:46:51 -0400

    HW Week 5

---
+++ Week5HW/Assets/ClickToMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class 
+    ClickToMove : MonoBehaviour {
+    float stopPos = 0;
+    public Vector3 destination;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Vector3.Distance(transform.position, destination) > stopPos)
+        {
+            rigidbody.AddForce(Vector3.Normalize(destination - transform.position) * 10f);
+        }
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit = new RaycastHit();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(mouseRay, out rayHit, 10000f))
+            {
+                destination = rayHit.point;
+            }
+        }
+	}
+}
---
+++ Week5HW/Assets/NPCBOT.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class 
+    NPCBOT : MonoBehaviour {
+    int randomnumber = 0;
+
+	// Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Physics.Raycast(transform.position, transform.forward))
+        {
+            randomnumber = Random.Range(0, 10);
+            if (randomnumber < 5)
+            {
+                transform.eulerAngles = new Vector3(0, 90, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, -90, 0);
+            }
+        }
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(transform.forward);
+        //could also use "rigidbody.AddRelativeForce(Vector3.forward);"
+        
+
+    }
+}
