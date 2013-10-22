9101f7cf2775e89814a7246bf7d6ae87c2894695
Matt Chen <matt.chen312@gmail.com>
2013-10-22 12:02:47 -0400

    Game feel. Using sounds

---
+++ Week8/Assets/ballclack.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class 
+    ballclack : MonoBehaviour {
+    
+    public Vector3 start, end;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        StartCoroutine( BallMove() );
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+    IEnumerator BallMove()
+    {
+        while (true)
+        {
+            float t = Mathf.Abs(Mathf.Sin(Time.time));
+            transform.position = Vector3.Lerp(start, end, t);
+            if (t > .49f && t < .51f && audio.isPlaying == false)
+            {
+                audio.Play();
+            }
+        }
+    }
+
+}
