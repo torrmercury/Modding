d74f598a7ea7c8fd4fb6df31f719bf90f597a4c7
Matt Chen <matt.chen312@gmail.com>
2013-10-28 19:40:34 -0400

    Week 8 and HW

---
+++ Week8 HW/Assets/Scripts/Platformer.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//3D platformer example: Mario 64
+//3rd person cam: parent the camera behind the player
+public class Platformer : MonoBehaviour
+{
+    Vector3 inputVector;
+    float speed = 5f;
+    float turnSpeed = 120f;
+    float jumpSpeed = 2f;
+    float fallSpeed = 6f;
+    bool grounded = false;
+    float movement;
+    float turning;
+
+    void Update()
+    {
+        /*  All of this is the usage of Input.GetKey. Replacing with GetAxis.
+        //We're putting the entirity of the player movement input into
+        //inputVector before moving it to FixedUpdate (which moves the player)
+
+            //MOVE FORWARD
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += transform.forward;
+        }
+
+            //MOVE BACKWARD
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += -transform.forward;
+        }
+        
+            //turning left and right
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, -turnSpeed * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+        }
+
+        */
+
+
+        inputVector = Vector3.zero;
+
+        //This is the usage of Input.GetAxis
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            movement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            transform.Translate(0, 0, movement);
+        }
+
+        turning = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        //Jumping
+        if (Physics.Raycast(transform.position, -transform.up, 1.3f) == true)
+        {
+            grounded = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                inputVector += Vector3.up * jumpSpeed;
+            }
+        }
+        else
+        {
+            grounded = false;
+        }
+
+
+        //Debug.Log(Input.GetAxis("Vertical"));
+        //Debug.Log(movement);
+
+    }
+    
+    void FixedUpdate()
+    {
+        
+        //Applying physics
+        if (inputVector != Vector3.zero)
+        {
+            rigidbody.AddForce(inputVector * speed, ForceMode.VelocityChange);
+        }
+
+        else
+        {
+            rigidbody.AddForce(-rigidbody.velocity + Physics.gravity * fallSpeed, ForceMode.Acceleration);
+        }
+        
+
+        //Usage of GetAxis turning
+        transform.Rotate(0, turning, 0);
+        
+        //movement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        
+    }
+}
--- Week8/Assets/ballclack.cs
+++ Week8/Assets/ballclack.cs
@@ -6,11 +6,13 @@ public class
     
     public Vector3 start, end;
 
+    Vector3 baseCameraPosition;
 
 	// Use this for initialization
 	void Start () 
     {
         StartCoroutine( BallMove() );
+        baseCameraPosition = Camera.main.transform.position;
 	}
 	
 	// Update is called once per frame
@@ -18,6 +20,22 @@ public class
     {
         
 	}
+
+
+    IEnumerator ScreenShake()
+    {
+        float t = 1;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            Camera.main.transform.position = baseCameraPosition + t *
+                                             new Vector3(Mathf.Sin(Time.time * 10f),
+                                                         Mathf.Sin(Time.time * 12.5f),
+                                                         Mathf.Sin(Time.time * 7f));
+            yield return 0;
+        }
+    }
+    
     IEnumerator BallMove()
     {
         while (true)
@@ -27,8 +45,13 @@ public class
             if (t > .49f && t < .51f && audio.isPlaying == false)
             {
                 audio.Play();
+                StartCoroutine(ScreenShake());
             }
+            yield return 0;
         }
     }
+
+    //implenenting screen shake in a super hacky way
+
 
 }
