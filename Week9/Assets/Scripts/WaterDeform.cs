5d116c3c5b1d651d6a8b76ecfa4ce60b4edfc4e5
Matt Chen <matt.chen312@gmail.com>
2013-11-02 18:12:40 -0400

    Thursday Class

---
+++ Week9/Assets/Scripts/WaterDeform.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterDeform : MonoBehaviour {
+    MeshFilter mf;
+    Vector3[] baseVertices;
+    Vector3[] workingCopy;
+    float waveHeight = .001f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        mf = GetComponent<MeshFilter>();
+        baseVertices = mf.mesh.vertices;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //every frame starts with a fresh copy of the untouched plane
+        workingCopy = baseVertices; 
+        for (int i = 0; i < workingCopy.Length; i++)
+        {
+            workingCopy[i] = baseVertices[i] + Vector3.up * Mathf.Sin(Time.time + i) * waveHeight;
+        }
+
+        mf.mesh.vertices = workingCopy;
+
+        //normals to see directions
+        mf.mesh.RecalculateNormals();
+        for (int i = 0; i<mf.mesh.vertices.Length;i++)
+        {
+            Debug.DrawRay(mf.mesh.vertices[i], mf.mesh.normals[i]);
+        }
+	}
+}
---
+++ Week9/Assets/Scripts/WaterScrolling.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterScrolling : MonoBehaviour {
+    float scrollSpeed = 0.2F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update() 
+    {
+        float offset = Time.time * scrollSpeed;
+        renderer.material.mainTextureOffset = new Vector2(Mathf.Sin(offset), offset);
+	}
+}
