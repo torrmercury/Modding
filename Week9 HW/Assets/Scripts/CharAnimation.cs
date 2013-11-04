b44ef41ee0ffb73ada49514b393b8275793ca3a5
torrmercury <matt.chen312@gmail.com>
2013-11-03 20:58:03 -0500

    Week 9 HW Started

---
+++ Week9 HW/Assets/Scripts/CharAnimation.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CharAnimation : MonoBehaviour {
+
+    public Rigidbody myrigidbody; //using this to make walking go as fast as the player is walking
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if player is moving or holding down any buttons then play walk
+        //if not, crossfade to idle
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            animation["Walk"].speed = myrigidbody.velocity.magnitude;
+            //animation.CrossFade("Walk", .5f);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animation["Jump"].speed = myrigidbody.velocity.magnitude;
+        }
+        else
+        {
+            animation.CrossFade("Idle", .5f);
+        }
+	}
+}
