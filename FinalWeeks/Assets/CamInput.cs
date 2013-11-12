39d0518eba6eca503e3ddbbf228d69cee76ac2e8
Matt Chen <matt.chen312@gmail.com>
2013-11-12 12:09:27 -0500

    Final few weeks of classwork

---
+++ FinalWeeks/Assets/CamInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamInput : MonoBehaviour {
+
+    public Sheeeeeeep dolly;
+    public TextMesh fameMeter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayhit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out rayhit, 10000f))
+        {
+            if (rayhit.collider.tag == "sheep" && Input.GetMouseButtonDown(0))
+            {
+                Sheeeeeeep selectedSheep = rayhit.collider.GetComponent<Sheeeeeeep>();
+                fameMeter.text = "Sheep's fame is " + selectedSheep.fame.ToString();
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Sheeeeeeep newSheep = Instantiate(dolly, rayhit.point, Quaternion.identity) as Sheeeeeeep;
+                newSheep.fame = Random.Range(1, 100);
+            }
+        }
+        
+        
+	}
+}
---
+++ FinalWeeks/Assets/Sheeeeeeep.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sheeeeeeep : MonoBehaviour {
+    public int fame = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
